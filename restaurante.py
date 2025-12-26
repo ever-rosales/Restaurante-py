f94e2d9238eafa65f22c3b7b7187124a5b971ab9
Ever <daftsure9@gmail.com>
2025-12-25 19:14:19 -0600

    Agregando los elementos a una lista

--- restaurante.py
+++ restaurante.py
@@ -1,7 +1,10 @@
-#Creando un restaurante
-#un restaurante cuenta con comida y bebidas
-print("******Bienvenido******")
-print("A continuación se encuentran los platillos disponibles")
+#CAMBIOS
+#LOS ELEMENTOS SERÁN INTRODUCIDOS A UNA LISTA PARA PODER MANEJARLO CON MAYOR FACILIDAD
+comidaLista = ["Hamburguesa Clasica", "Tacos", "Enchiladas Suizas", "Ensaldada de Pollo", "Pasta Alfredo", "Club Sandwich", "Sopa Azteca", "Salmon a la Plancha", "Boneless de Pollo" ]
+comidaPrecios = [170,100,130,125,140,130,85,280,150]
+bebidasLista = ["Refresco", "Cerveza", "Vino", "Agua"]
+bebidasPrecios = [30,80,100,20]
+print("A continuacion se muestran los platillos disponibles")
 print("1. Hamburguesa Clasica---> $170")
 print("2. Tacos (orden de 5)---> $100 ")
 print("3. Enchiladas Suizas ---> $130 ")
@@ -12,40 +15,18 @@ print("7. Sopa Azteca---> $85 ")
 print("8. Salmon a la plancha---> $280")
 print("9. Boneless de Pollo---> $150")
 comida = int(input("Selecciona una opcion: "))
-if (comida == 1):
-    print("Preparando Hamburguesa Clasica...")
-elif (comida==2):
-    print("Preparando orden de tacos")
-elif (comida==3):
-    print("Preparando enchiladas suizas")
-elif (comida==4):
-    print("Preparando ensalada de pollo")
-elif (comida==5):
-    print("Preparando pasta Alfredo")
-elif (comida==6):
-    print("Preparado club sandwich")
-elif (comida==7):
-    print("Preparando sopa azteca")
-elif (comida==8):
-    print("Preparando salmon a la plancha")
-elif (comida==9):
-    print("Preparando boneless de pollo")
-else:
-    print("Selecciona una opcion correcta")
+print("A continuacion se muestran los bebidas disponibles")
+print("1. Refresco--->$30")
+print("2. Cerveza--->$80")
+print("3. Vino--->$100")
+print("4. Agua--->$20")
+bebida = int(input("Selecciona una opcion: "))
+#conviertiendo el valor obtenido a valor entero
+comidaIndice = int(comida)-1
+bebidaIndice = int(bebida)-1
+precioComida = {comidaPrecios[comidaIndice]}
+precioBebida = {bebidasPrecios[bebidaIndice]}
+print("Preparando ", {comidaLista[comidaIndice]}, precioComida )
+print("Preparando ", {bebidasLista[bebidaIndice]}, precioBebida)
 
-print("A continuacion se muestran las bebidas disponibles")
-print("1. Refresco")
-print("2. Cerveza")
-print("3. Vino")
-print("4. Agua")
-bebida=int(input("Selecciona una opcion: "))
-if(bebida==1):
-    print("Preparando refresco")
-elif(bebida==2):
-    print("Preparando cerveza")
-elif(bebida==3):
-    print("Preparando vino")
-elif (bebida==4):
-    print("Preparando agua")
-else:
-    print("Selecciona una opcion valida")
+
